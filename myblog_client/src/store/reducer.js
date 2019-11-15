b431ae4686e58616f3d4b71613b2a23fe61ed7a3
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-15 21:16:08 +1100

    combine reducers

--- myblog_client/src/store/reducer.js
+++ myblog_client/src/store/reducer.js
@@ -1,15 +1,8 @@
-const defaulState = {
-  focused: false
-};
+import { combineReducers } from "redux";
+import { reducer as headerReducer } from "../common/header/store";
 
-export default (state = defaulState, action) => {
-  if (action.type === "search_focus") {
-    return { focused: true };
-  }
+const reducer = combineReducers({
+  header: headerReducer
+});
 
-  if (action.type === "search_blur") {
-    return { focused: false };
-  }
-
-  return state;
-};
+export default reducer;
