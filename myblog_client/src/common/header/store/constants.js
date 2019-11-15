91213a99c9b704f4d6850716813649e68365813f
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-15 21:45:04 +1100

    split actionCreators and constants

---
+++ myblog_client/src/common/header/store/constants.js
@@ -0,0 +1,2 @@
+export const SEATCH_FOCUS = "header/SEATCH_FOCUS";
+export const SEATCH_BLUR = "header/SEATCH_BLUR";
--- myblog_client/src/common/header/store/reducer.js
+++ myblog_client/src/common/header/store/reducer.js
@@ -1,13 +1,15 @@
+import * as constants from "./constants";
+
 const defaulState = {
   focused: false
 };
 
 export default (state = defaulState, action) => {
-  if (action.type === "search_focus") {
+  if (action.type === constants.SEATCH_FOCUS) {
     return { focused: true };
   }
 
-  if (action.type === "search_blur") {
+  if (action.type === constants.SEATCH_BLUR) {
     return { focused: false };
   }
 
