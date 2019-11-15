8d108e711168699d4d2d0925750da642df9d312f
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-15 22:02:36 +1100

    use immutable.js to ensure state immutable

--- myblog_client/src/common/header/store/reducer.js
+++ myblog_client/src/common/header/store/reducer.js
@@ -1,16 +1,18 @@
 import * as constants from "./constants";
+import { fromJS } from "immutable";
 
-const defaulState = {
+const defaulState = fromJS({
   focused: false
-};
+});
 
 export default (state = defaulState, action) => {
   if (action.type === constants.SEATCH_FOCUS) {
-    return { focused: true };
+    //immutable对象的set方法，会结合之前immutable对象的值和设置的值，返回一个全新的对象
+    return state.set("focused", true);
   }
 
   if (action.type === constants.SEATCH_BLUR) {
-    return { focused: false };
+    return state.set("focused", false);
   }
 
   return state;
