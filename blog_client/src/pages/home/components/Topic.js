141751c9b532e86d2f8ae2758f6a4d51eeb14ef8
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-17 11:27:16 +1100

    add topic items

--- blog_client/src/pages/home/components/Topic.js
+++ blog_client/src/pages/home/components/Topic.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { TopicWrapper, TopicItem } from "../style.js";
+import { connect } from "react-redux";
 
 class Topic extends Component {
   render() {
-    return <div>Topic</div>;
+    const { list } = this.props;
+    return (
+      <TopicWrapper>
+        {list.map(item => (
+          <TopicItem key={item.get("id")}>
+            <img className="topic-pic" src={item.get("imgUrl")} />
+            {item.get("title")}
+          </TopicItem>
+        ))}
+      </TopicWrapper>
+    );
   }
 }
-export default Topic;
+
+const mapStateToProps = state => ({
+  list: state.get("home").get("topicList")
+});
+
+export default connect(mapStateToProps, null)(Topic);
--- blog_client/src/pages/home/style.js
+++ blog_client/src/pages/home/style.js
@@ -21,3 +21,29 @@ export const HomeRight = styled.div`
   width: 240px;
   float: right;
 `;
+export const TopicWrapper = styled.div`
+  padding: 20px 0 10px 0;
+  overflow: hidden;
+  margin-left: -18px;
+`;
+
+export const TopicItem = styled.div`
+  float: left;
+  background: #f7f7f7;
+  padding-right: 10px;
+  margin-left: 18px;
+  height: 32px;
+  line-height: 32px;
+  font-size: 14px;
+  color: #000;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  margin-bottom: 18px;
+  .topic-pic {
+    display: block;
+    margin-right: 10px;
+    float: left;
+    width: 32px;
+    height: 32px;
+  }
+`;
